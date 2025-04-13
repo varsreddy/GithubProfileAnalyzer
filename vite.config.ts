823819027e7c0483ec1114244017b7855f023d9e
varsreddy <varshithak809@gmail.com>
2025-04-13 10:38:37 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
